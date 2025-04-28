9fac1c0845183fb228b8760dd776fb17e4b2fa3a
PHinterbauer <phinterbauer.privat@gmail.com>
2025-04-28 11:27:31 +0200

    trying für code aber nix funktionieren

--- Robotersteuerung_HIL.cpp
+++ Robotersteuerung_HIL.cpp
@@ -3,10 +3,23 @@
 
 int main()
 {
+    stdio_init_all();
+
     float x, y, z;
     int grabber;
-    // Float for x, y and z movement and int for grabber (0 - closed, 1 - open)
-    scanf("x %f, y %f, z %f, grabber %d\n", &x, &y, &z, &grabber);
 
-    printf("x: %f, y: %f, z: %f, grabber: %d\n", x, y, z, grabber);
+    printf("Bitte Werte eingeben im Format: x <float>, y <float>, z <float>, grabber <0 oder 1>\n");
+
+    if (scanf("x %f, y %f, z %f, grabber %d", &x, &y, &z, &grabber) != 4) {
+        printf("Fehlerhafte Eingabe!\n");
+        return 1;  // Fehlercode
+    }
+
+    printf("Eingabewerte:\n");
+    printf("x: %f\n", x);
+    printf("y: %f\n", y);
+    printf("z: %f\n", z);
+    printf("grabber: %d\n", grabber);
+
+    return 0;
 }
