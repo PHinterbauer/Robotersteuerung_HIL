b068cff23caa13d7cffb34cfca70997c5563e6ca
Paul Hinterbauer <phinterbauer.privat@gmail.com>
2025-04-29 19:15:47 +0200

    trying semaphor send help pls

--- Robotersteuerung_HIL.cpp
+++ Robotersteuerung_HIL.cpp
@@ -27,59 +27,73 @@ void get_user_input(void* nothing)
     printf("Enter x, y, z coordinates and grabber state (0 or 1) in following format: \n");
     printf("x <float>, y <float>, z <float>, grabber <int>\n");
     printf("Press Enter to submit your command. And enter \"run\" to execute saved commands.\n");
-    if (scanf("x %f, y %f, z %f, grabber %d", &x, &y, &z, &grabber) == 4)
-    {
-        if (command_count < MAX_COMMANDS) 
-        {
-            x_commands[command_count] = x;
-            y_commands[command_count] = y;
-            z_commands[command_count] = z;
-            grabber_commands[command_count] = grabber;
-            command_count++;
-        } 
-        else 
-        {
-            printf("Command buffer full. Cannot save more commands.\n");
-        }
-    } 
-    else 
-    {
-        if (scanf("%s", command) == 1)
-        {
-            if (strcmp(command, "run") == 0)
-            {
-                run = 1;
-            }
-        } 
-        else
-        {
-            printf("Invalid command. Please try again.\n");
-        }
-    }
-    vTaskDelay(pdMS_TO_TICKS(100));
-}
 
-void execute_commands(void* nothing)
-{
     while (1)
     {
-        if (run == 1)
+        if (xSemaphoreTake(mutex, portMAX_DELAY))
         {
-            for (int i = 0; i < command_count; i++)
+            if (scanf("x %f, y %f, z %f, grabber %d", &x, &y, &z, &grabber) == 4)
             {
-                printf("Executing command %d: x = %f, y = %f, z = %f, grabber = %d\n", i + 1, x_commands[i], y_commands[i], z_commands[i], grabber_commands[i]);
+                if (command_count < MAX_COMMANDS) 
+                {
+                    x_commands[command_count] = x;
+                    y_commands[command_count] = y;
+                    z_commands[command_count] = z;
+                    grabber_commands[command_count] = grabber;
+                    command_count++;
+                } 
+                else 
+                {
+                    printf("Command buffer full. Cannot save more commands.\n");
+                }
+            } 
+            else 
+            {
+                if (scanf("%s", command) == 1)
+                {
+                    if (strcmp(command, "run") == 0)
+                    {
+                        run = 1;
+                    }
+                } 
+                else
+                {
+                    printf("Invalid command. Please try again.\n");
+                }
             }
-            run = 0; // Reset run flag after executing commands
-            command_count = 0; // Clear command buffer after execution
+            xSemaphoreGive(mutex);
         }
+        vTaskDelay(pdMS_TO_TICKS(100));
     }
-    vTaskDelay(pdMS_TO_TICKS(100));
+
+    void execute_commands(void* nothing)
+    {
+        while (1)
+        {
+            if (xSemaphoreTake(mutex, portMAX_DELAY))
+            {
+                if (run == 1)
+                {
+                    for (int i = 0; i < command_count; i++)
+                    {
+                        printf("Executing command %d: x = %f, y = %f, z = %f, grabber = %d\n", i + 1, x_commands[i], y_commands[i], z_commands[i], grabber_commands[i]);
+                    }
+                    run = 0; // Reset run flag after executing commands
+                    command_count = 0; // Clear command buffer after execution
+                }
+                xSemaphoreGive(mutex);
+            }
+        }
+        vTaskDelay(pdMS_TO_TICKS(100));
+    }
 }
 
 int main()
 {
     stdio_init_all();
     sleep_ms(2000); // Wait for the serial connection to establish
+
+    mutex = xSemaphoreCreateMutex();
 
     printf("===========================\n");
     printf("Robot Arm Control Pannel\n");
